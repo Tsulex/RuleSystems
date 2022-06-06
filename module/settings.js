e87ba6825af15a47341b893c8bfafb4e51983eab
Tsulex <alexete.acb@gmail.com>
2022-06-06 17:01:02 +0200

    changes

--- module/settings.js
+++ module/settings.js
@@ -1,4 +1,13 @@
 export const registerSettings = function () {
     // Register any custom module settings here
     let modulename = "ruleSystems";
+
+    game.settings.register(modulename, "craftingBook", {
+        name: "Crafting Book",
+        hint: "Activa el Crafting Book",
+        default: false,
+        type: Boolean,
+        scope: 'world',
+        config: true
+    });
 }
