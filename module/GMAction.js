e28f617072adeb5f5613680091a2816d73db24c6
Tsulex <alexete.acb@gmail.com>
2022-08-06 22:56:51 +0200

    AAAAAAAA

--- module/GMAction.js
+++ module/GMAction.js
@@ -2,6 +2,9 @@ export var socketlibSocket = undefined;
 
 export let setupSocket = () => {
     socketlibSocket = globalThis.socketlib.registerModule("RuleSystems");
+    socketlibSocket.register("PokerPlayerDiscardDialog", PokerPlayerDiscardDialog);
+    socketlibSocket.register("PokerHandsDialog", PokerHandsDialog);
+    socketlibSocket.register("PokerDialog", PokerDialog);
     socketlibSocket.register("MVPDialog", MVPDialog);
     socketlibSocket.register("MVPDialogUpdate", MVPDialogUpdate);
     socketlibSocket.register("cookingMama", cookingMama);
@@ -9,18 +12,57 @@ export let setupSocket = () => {
     socketlibSocket.register("playVS", playVS);
 }
 
+/*5 CARTAS - APUESTA 1 VEZ (A IGUALAR LA MAS ALTA) - DESCARTE DE CARTAS - APUESTA AL ALZA (RENDICION / IGUALAR) - FIN*/
+
+/* ------------------------------------ */
+/* Poker System Player Discard          */
+/* ------------------------------------ */
+async function PokerPlayerDiscardDialog(charname, discard){
+    const journal = game.journal.getName("Poker Game");
+    const pokerData = JSON.parse(journal.data.content);
+    discard.forEach(x => {
+        pokerData.hands[charname].cards[x] = pokerData.deck.pop();
+    })
+    journal.update({content : JSON.stringify(pokerData)});
+    socketlibSocket.executeForUsers("PokerDialog", [pokerData.hands[charname].id], pokerData.hands[charname].cards);
+}
+
+/* ------------------------------------ */
+/* Poker System Hands                   */
+/* ------------------------------------ */
+async function PokerHandsDialog(hands){
+
+    const PokerDialog = new Dialog({
+        title: "Poker Game - Players Hands",
+        content: `${JSON.stringify(hands)}`,
+        buttons: {}
+        //aqui llamar a PokerPlayerDiscardDialog (pasar charname)
+    }).render(true);
+}
+
+/* ------------------------------------ */
+/* Poker System Players                 */
+/* ------------------------------------ */
+async function PokerDialog(cards){
+
+    const PokerDialog = new Dialog({
+        title: "Poker Game",
+        content: `${cards[0]} - ${cards[1]} - ${cards[2]} - ${cards[3]} - ${cards[4]}`,
+        buttons: {}
+    }).render(true);
+}
 
 /* ------------------------------------ */
 /* 		*/
 /* ------------------------------------ */
 async function MVPDialog(){
-    const nvpDialog = new Dialog({
+    const mvpDialog = new Dialog({
         title: "MVP",
         content: `<style>.mvp-name{background: rgba(0,0,0,0.5);font-size: xxx-large;vertical-align: -350px; padding: 0.05em 0.3em; color:white } .mvp-avatar:hover{opacity: 1; box-shadow: 0 0 10px white !important;} .mvp-avatar{opacity: 0.75}}</style><div style="display: flex;flex-direction: row;min-height: 500px;">${game.users.filter(x => !x.isOwner && x.character && x.active && !x.isGM).map(x => `<button class="mvp-avatar" value="${x.id}" style="background:url(${x.avatar}); background-repeat: no-repeat; background-position: center;"/><span class="mvp-name">${x.name}</span>`).join("")}</div>`,
         buttons: {},
         render: html => {
             html.find('.mvp-avatar').on("click", (ev) => {
-                nvpDialog.close();
+                mvpDialog.close();
                 socketlibSocket.executeAsGM("MVPDialogUpdate", ev.target.value);
             })
         }
